b433f9ef67d5c3bdcd3e3e26f7baffa1be75628a
Qiong Zhou Huang <qiongzhouh@gmail.com>
2022-04-15 21:16:30 -0400

    new course classes

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { TextField, Grid, Box, Typography, Container } from '@mui/material'
+
+import classes from '../public/eecs-renumbering.json'
+
+const subjects = classes.subjects
+
+export default function Index() {
+  const [ searchStr, setSearchStr ] = React.useState('')
+
+  const handleChange = (event) => {
+    setSearchStr(event.target.value);
+  };
+
+  const filteredClasses = subjects.filter(subject => subject.oldnum.includes(searchStr) || 
+    subject.newnum.includes(searchStr) || subject.title.includes(searchStr) || searchStr.length == 0)
+
+  const classesElements = filteredClasses.map((element, index) => (
+    <Grid item key={index} xs={6} sm={4} lg={3}>
+      <Box bgcolor="#d0f7c1" key={index} m={1} p={1}>
+        <Typography variant="h5">{`${element.newnum}, formerly ${element.oldnum}`}</Typography>
+        <Typography variant="h5"></Typography>
+        <Typography variant="h6">{element.title}</Typography>
+        <Typography>{`(${element.level}) ${element.subarea}`}</Typography>
+      </Box>
+    </Grid>
+  ))
+
+  return (
+    <Container maxWidth="xl">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          New Course 6 Classes Search
+        </Typography>
+        <TextField id="outlined-basic" fullWidth={true} label="Search (e.g. 6.033)" value={searchStr} variant="filled" onChange={handleChange}/>
+        {/* {JSON.stringify(filteredClasses)} */}
+        <Grid container>
+          {classesElements}
+        </Grid>
+      </Box>
+    </Container>
+  );
+}
